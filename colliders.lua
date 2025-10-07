b7292aa6f13a69e900e4c93914b21836fe4048e2
Cameron Kelly <cameron.kelly@utdallas.edu>
2025-10-07 13:50:18 -0500

    collider behavior

---
+++ colliders.lua
@@ -0,0 +1,208 @@
+collider = class:new()
+
+function collider:init(type, points)
+    self.type = "none"
+    self.points = {}
+end
+
+-- move collider such that the first point lies at (x, y)
+function collider:move(x, y)
+    if self.type == "none" then return end
+
+    local dx = x - self.points[1]
+    local dy = y - self.points[2]
+    for i = 1, #self.points, 2 do
+        self.points[i] = self.points[i] + dx
+        self.points[i+1] = self.points[i+1] + dy
+    end
+end
+
+function collider:draw()
+    for i = 1, #self.points - 2, 2 do
+        love.graphics.line(self.points[i], self.points[i+1], self.points[i+2], self.points[i+3])
+    end
+    love.graphics.line(self.points[#self.points-1], self.points[#self.points], self.points[1], self.points[2])
+end
+
+pointcollider = collider:new()
+function pointcollider:init(x, y)
+    self.type = "point"
+    self.points = {x, y}
+end
+function pointcollider:draw()
+    love.graphics.circle("fill", self.points[1], self.points[2], 3)
+end
+
+linecollider = collider:new()
+function linecollider:init(x1, y1, x2, y2)
+    self.type = "line"
+    self.points = {x1, y1, x2, y2}
+end
+
+rectcollider = collider:new()
+function rectcollider:init(x, y, w, h)
+    self.type = "rect"
+    self.points = {x, y, x+w, y, x+w, y+h, x, y+h}
+    self.quad = {x, y, w, h}
+end
+function rectcollider:move(x, y)
+    self.quad[1] = x
+    self.quad[2] = y
+
+    local dx = x - self.points[1]
+    local dy = y - self.points[2]
+    for i = 1, #self.points, 2 do
+        self.points[i] = self.points[i] + dx
+        self.points[i+1] = self.points[i+1] + dy
+    end
+end
+
+polycollider = collider:new()
+function polycollider:init(points)
+    self.type = "poly"
+    self.points = points
+end
+
+function intersect(a, b)
+    if a.type == "none" then
+        return false
+    elseif a.type == "point" then
+        if b.type == "point" then
+            return false
+        elseif b.type == "line" then
+            return false
+        elseif b.type == "rect" then
+            return pointInRect(a, b)
+        elseif b.type == "poly" then
+            return pointInPoly(a, b)
+        end
+    elseif a.type == "line" then
+        if b.type == "line" then
+            return lineIntersect(a, b)
+        elseif b.type == "rect" then
+            return linePolyIntersect(a, b)
+        elseif b.type == "poly" then
+            return linePolyIntersect(a, b)
+        end
+    elseif a.type == "rect" then
+        if b.type == "rect" then
+            return rectIntersect(a, b)
+        elseif b.type == "poly" then
+            return polyIntersect(a, b)
+        end
+    elseif a.type == "poly" then
+        if b.type == "poly" then
+            return polyIntersect(a, b)
+        end
+    else
+        error("Intersection test with an invalid collider type")
+    end
+
+    return intersect(b, a)
+end
+
+--returns true if any point in a is inside the rect of b
+function pointInRect(a, b)
+    for i = 1, #a.points, 2 do
+        if a.points[i] >= b.quad[1] and
+           a.points[i+1] >= b.quad[2] and
+           a.points[i] <= b.quad[1] + b.quad[3] and
+           a.points[i+1] <= b.quad[2] + b.quad[4]
+        then return true end
+    end
+    return false
+end
+
+--returns true if any point in a is inside the polygon of b
+function pointInPoly(a, b)
+    for i = 1, #a.points, 2 do
+        local rayIntersects = 0
+        local line = {a.points[i], a.points[i+1], 100000, a.points[i+1]}
+        for j = 1, #b.points, 2 do
+            local x, y, int = lineTest(line,
+                {b.points[wrap(j, #b.points)], b.points[wrap(j+1, #b.points)], b.points[wrap(j+2, #b.points)], b.points[wrap(j+3, #b.points)]}
+            )
+            if int then rayIntersects = rayIntersects + 1 end
+        end
+        if rayIntersects % 2 == 1 then return true end
+    end
+    return false
+end
+
+--returns true if the line a intersects the line b
+function lineIntersect(a, b)
+    local x, y, int = lineTest(a.points, b.points)
+    return int
+end
+
+--returns true if the line a intersects the polygon b
+function linePolyIntersect(a, b)
+    --line tests
+    for i = 1, #b.points, 2 do
+        local x, y, int = lineTest(a.points,
+            {b.points[wrap(i, #b.points)], b.points[wrap(i+1, #b.points)], b.points[wrap(i+2, #b.points)], b.points[wrap(i+3, #b.points)]}
+        )
+        if int then return true end
+    end
+
+    --point in polygon tests
+    return pointInPoly(a, b)
+end
+
+--returns true if the polygon a intersects the polygon b
+function polyIntersect(a, b)
+    --line tests
+    for i = 1, #a.points, 2 do
+        for j = 1, #b.points, 2 do
+            local x, y, int = lineTest(
+                {a.points[wrap(j, #a.points)], a.points[wrap(j+1, #a.points)], a.points[wrap(j+2, #a.points)], a.points[wrap(j+3, #a.points)]},
+                {b.points[wrap(j, #b.points)], b.points[wrap(j+1, #b.points)], b.points[wrap(j+2, #b.points)], b.points[wrap(j+3, #b.points)]}
+            )
+            if int then return true end
+        end
+    end
+
+    --point in polygon tests
+    return pointInPoly(a, b)
+end
+
+--returns true if the rect a intersects the rect b
+function rectIntersect(a, b)
+    -- love.graphics.setBackgroundColor(1, 1, 1, 1)
+    return a.quad[1] <= b.quad[1] + b.quad[3] and
+           a.quad[2] <= b.quad[2] + b.quad[4] and
+           a.quad[1] + a.quad[3] >= b.quad[1] and
+           a.quad[2] + a.quad[4] >= b.quad[2]
+end
+
+--takes in two lines {x1, y1, x2, y2} and returns the (x, y) position of the intersection and a bool if they intersect within the segment
+function lineTest(lineA, lineB)
+    if math.abs(lineA[1] - lineA[3]) < 0.0001 then
+        lineA[1] = lineA[1] + 0.0001
+    end
+    if math.abs(lineB[1] - lineB[3]) < 0.0001 then
+        lineB[1] = lineB[1] + 0.0001
+    end
+
+    local m1 = (lineA[2] - lineA[4]) / (lineA[1] - lineA[3])
+    local b1 = -m1*lineA[1] + lineA[2]
+
+    local m2 = (lineB[2] - lineB[4]) / (lineB[1] - lineB[3])
+    local b2 = -m2*lineB[1] + lineB[2]
+
+    --parallel lines
+    if math.abs(m1 - m2) < 0.0001 then
+        return false
+    end
+
+    local x = (b2 - b1) / (m1 - m2)
+    local y = m1*x + b1
+    local int = x >= math.min(lineA[1], lineA[3]) and x <= math.max(lineA[1], lineA[3]) and
+                x >= math.min(lineB[1], lineB[3]) and x <= math.max(lineB[1], lineB[3])
+
+    return x, y, int
+end
+
+function wrap(x, a)
+    return (x - 1) % a + 1
+end
--- main.lua
+++ main.lua
@@ -1,13 +1,27 @@
 require "class"
+require "colliders"
 
 function love.load()
-
+    a = polycollider:new({20, 0, 80, 0, 100, 20, 100, 80, 80, 100, 20, 100, 0, 80, 0, 20})
+    b = linecollider:new(300, 100, 400, 500)
+    c = pointcollider:new(100, 100)
+    d = rectcollider:new(100, 300, 30, 30)
+    love.graphics.setBackgroundColor(0, 0, 0)
 end
 
 function love.update()
-
+    a:move(love.mouse.getPosition())
 end
 
 function love.draw()
-
+    if intersect(a, b) or intersect(a, c) or intersect(a, d) then
+        love.graphics.setColor(1, 1/2, 1/2, 1)
+    else
+        love.graphics.setColor(1, 1, 1, 1)
+    end
+    a:draw()
+    b:draw()
+    c:draw()
+    d:draw()
+    love.graphics.print(b.type, 0, 0)
 end
