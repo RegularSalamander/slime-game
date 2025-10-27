6788f6b209d8b9bd80c8e04278a7f73b5f0a91f9
Cameron Kelly <cameron.kelly@utdallas.edu>
2025-10-27 15:46:39 -0500

    better movement tweaks and new palette

--- game.lua
+++ game.lua
@@ -11,8 +11,7 @@ function game_load()
     gamePlayer = player:new()
 
     camera = {x = 0, y = 0}
-
-    love.graphics.setBackgroundColor(41/255, 173/255, 255/255)
+    love.graphics.setBackgroundColor(60/255, 159/255, 156/255)
 end
 
 function game_update()
